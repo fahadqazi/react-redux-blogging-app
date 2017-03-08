ec8848aacb4af9c20c45f94d1b2edf6e0e2c3d40
Fahad Qazi <fahadqazi@gmail.com>
2017-03-08 23:39:26 +0000

    add and use IndexRoute

---
+++ src/components/posts_index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PostIndex = () => {
+    return <div>List of Blog posts</div>
+}
+
+export default PostIndex;
--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,15 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
+import PostIndex from './components/posts_index';
+
+const Greeting = () => {
+    return <div>Hello there!!!</div>
+}
 
 export default(
-    <Route path="/" component={App} />
+    <Route path="/" component={App}>
+        <IndexRoute component={PostIndex} />
+        <Route path="greet" component={Greeting} />
+    </Route>
 );
