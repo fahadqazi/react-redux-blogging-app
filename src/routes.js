b0302e9e5be336ce042791918d7469b880ff5697
Fahad Qazi <fahadqazi@gmail.com>
2017-03-10 23:16:21 +0000

    navigation between pages, view individual posts

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import PostIndex from './components/posts_index';
 import PostsNew from './components/post_new';
+import PostsShow from './components/posts_show'
 
 const Greeting = () => {
     return <div>Hello there!!!</div>
@@ -13,6 +14,7 @@ export default(
     <Route path="/" component={App}>
         <IndexRoute component={PostIndex} />
         <Route path="greet" component={Greeting} />
-        <Route path="posts/new" component={PostsNew}/>
+        <Route path="posts/new" component={PostsNew} />
+        <Route path="posts/:id" component={PostsShow} />
     </Route>
 );
