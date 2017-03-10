64b75a064515ba21a1ad623e2627ff0c64a80eb6
Fahad Qazi <fahadqazi@gmail.com>
2017-03-10 18:45:11 +0000

    adding form post capability

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,25 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';   
+export const CREATE_POST = 'CREATE_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=fahadqazi12345';
 
 export function fetchPosts(){
-    console.log('starting axios')
     const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
-    console.log(request);
 
     return {
         type: FETCH_POSTS,
+        payload: request
+    }
+}
+
+export function createPost(props) {
+    const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props)
+
+    return {
+        action: CREATE_POST,
         payload: request
     }
 }
--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import { bindActionCreators } from 'redux';
 
 class PostsNew extends Component{
     render(){
+        const { fields: {title, categories, content }, handleSubmit } = this.props;
+
         return(
-            <div>create From</div>
+            <form obSubmit={handleSubmit}>
+                <h3>Create a New Post</h3>
+                <div className="form-group">
+                    <label>Title</label>
+                    <input type="text" className="form-control" {...title}/>
+                </div>
+                <div className="form-group">
+                    <label>Categories</label>
+                    <input type="text" className="form-control" {...categories}/>
+                </div>
+                <div className="form-group">
+                    <label>Content</label>
+                    <textarea className="form-control" {...content}></textarea>
+                </div>
+
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
         );
     }
 }
 
-export default PostsNew; 
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ : }, dispatch);
+}
+
+export default reduxForm({
+    form: 'PostsNewForm',
+    fields: ['title', 'categories', 'content']
+}, null, mapDispatchToProps)(PostsNew);
